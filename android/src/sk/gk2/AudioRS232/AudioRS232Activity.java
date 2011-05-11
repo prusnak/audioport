5ba3e0211700176ae2212120f37c68da189b99c0
Pavol Rusnak <stick@gk2.sk>
2011-05-11 12:23:12 +0200

    initial commit of android app

---
+++ android/src/sk/gk2/AudioRS232/AudioRS232Activity.java
@@ -0,0 +1,13 @@
+package sk.gk2.AudioRS232;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AudioRS232Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
